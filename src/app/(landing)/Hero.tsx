1efa4d928f91b9568fc86449e241137d8b859f5c
Mathias Lawson <mathiaslawson70@gmail.com>
2024-07-29 10:59:12 +0000

    display current user instance

--- src/app/(landing)/Hero.tsx
+++ src/app/(landing)/Hero.tsx
@@ -1,14 +1,25 @@
 "use client"
-
+import { useUser } from "@clerk/nextjs";
 
 
 export default function Hero() {
  
+
+  const {isLoaded, isSignedIn, user} = useUser();
+
+  if (!isLoaded || !isSignedIn) {
+    return null;
+  }
+
+  console.log(user)
+ 
+  
   
   return (
     <>
       
       <main className="flex min-h-screen flex-col items-center justify-center background-color text-black">
+      <div>Hello, {user.fullName} </div>
       <h2>Hello from the landing page</h2>
 
       </main>
--- src/app/page.tsx
+++ src/app/page.tsx
@@ -2,9 +2,10 @@ import { api, HydrateClient } from "~/trpc/server";
 import Hero from "./(landing)/Hero";
 
 
+
+
 export default async function Home() {
- 
-  
+
   return (
     <HydrateClient>    
       <main className="flex min-h-screen flex-col items-center justify-center background-color text-black">
