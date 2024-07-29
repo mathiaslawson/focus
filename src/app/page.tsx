ae38c6b2dc8c0f742b75073926fc3ba6340364e3
Mathias Lawson <mathiaslawson70@gmail.com>
2024-07-29 08:39:13 +0000

    dark/light mode

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -15,8 +15,8 @@ export default async function Home() {
   return (
     <HydrateClient>
       
-      <main className="flex min-h-screen flex-col items-center justify-center bg-gradient-to-b from-[#2e026d] to-[#15162c] text-white">
-      <ThemeSwitcher />
+      <main className="flex min-h-screen flex-col items-center justify-center background-color text-black">
+      <ThemeSwitcher /> Switch Theme
 
       </main>
     </HydrateClient>
